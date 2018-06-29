11e7daecc9f17672a8cef2d98561290d7cef1d83
dianalopz <dlauralr@icloud.com>
2018-06-29 18:23:49 -0500

    v2 sedes y generaciones primer intento ok

--- src/data.js
+++ src/data.js
@@ -12,8 +12,24 @@ const laboratoriaAPI = 'https://api.myjson.com/bins/b18p2'
 fetch(laboratoriaAPI).then((res) => {
     return res.json();
   }).then((data) => {
-    document.getElementById('generacion-uno-lima').innerHTML = Object.keys(data);
-    console.log(Object.keys(data));
+    //Imprime Sedes
+    document.getElementById('sede-lima').innerHTML = Object.keys(data)[0];
+    document.getElementById('sede-mex').innerHTML = Object.keys(data)[1];
+    document.getElementById('sede-snt').innerHTML = Object.keys(data)[2];
+    //Imprime generaciones lima
+    document.getElementById('generacion-uno-lima').innerHTML = Object.keys(data.lima.generacion)[2];
+    document.getElementById('generacion-dos-lima').innerHTML = Object.keys(data.lima.generacion)[0];
+    document.getElementById('generacion-tres-lima').innerHTML = Object.keys(data.lima.generacion)[1];
+    //Imprime generaciones mex
+    document.getElementById('generacion-uno-mex').innerHTML = Object.keys(data.mexico.generacion)[2];
+    document.getElementById('generacion-dos-mex').innerHTML = Object.keys(data.mexico.generacion)[0];
+    document.getElementById('generacion-tres-mex').innerHTML = Object.keys(data.mexico.generacion)[1];
+    //Imprime generaciones santiago
+    document.getElementById('generacion-uno-stg').innerHTML = Object.keys(data.santiago.generacion)[2];
+    document.getElementById('generacion-dos-stg').innerHTML = Object.keys(data.santiago.generacion)[0];
+    document.getElementById('generacion-tres-stg').innerHTML = Object.keys(data.santiago.generacion)[1];
+    //Aqui se imprimirian las alumnas
+    document.getElementById('alumnas').
   }).catch((error) => {
   console.log(error);
 })
